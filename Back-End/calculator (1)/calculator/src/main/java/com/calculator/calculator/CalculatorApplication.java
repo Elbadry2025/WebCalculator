ea241626d88c0fc126287e6c3d8c8393cde6edf9
Elbadry2025 <96208963+Elbadry2025@users.noreply.github.com>
2022-11-29 21:54:52 +0200

    Add files via upload

---
+++ Back-End/calculator (1)/calculator/src/main/java/com/calculator/calculator/CalculatorApplication.java
@@ -0,0 +1,15 @@
+package com.calculator.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+
+public class CalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorApplication.class, args);
+	}
+
+}
